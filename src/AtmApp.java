4c57030a2261ebeb10cb5378b9f7b020cc817fb9
sukruozbey <121983247+sukruozbey@users.noreply.github.com>
2023-02-23 18:46:00 +0300

    v2

--- src/AtmApp.java
+++ src/AtmApp.java
@@ -1,5 +1,7 @@
 public class AtmApp {
     public static void main(String[] args) {
         System.out.println("Bu bit atm projesidir.");
+        //version 2
+        System.out.println("Java is easy");
     }
 }
